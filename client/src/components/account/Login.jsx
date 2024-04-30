3da6edd208b34f20c2fdcebfcfa6a0c2b8a40c8d
Sabana <sabanasultana132@gmail.com>
2024-05-01 04:34:16 +0530

    "feat: added SignUp button"

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import { Box, TextField, Button } from "@mui/material";
 import "./Login.css";
+import { useState } from 'react';
 
 export default function Login() {
-  const imageURL = 'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
-  return (
-   <Box className="w-96 login-container">
-     <img src={imageURL} alt="Login"  className='img'/>
-     <Box className='wrapper'>
-      <TextField id="standard-basic"  variant="standard" />
-      <TextField id="standard-basic"  variant="standard" />
-      <Button variant="contained" className='btn '>Login</Button>
-      <p className='text-center pt-2'>OR</p>
-      <Button variant="text" className=''>CREATE AN ACCOUNT</Button>
-     </Box>
-   </Box>
-  )
+    const imageURL = 'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
+    const [account,setAccount]=useState('login')
+    const signUpHandler=()=>{
+      account==='login'?setAccount('signup'):setAccount('login')
+    }
+   
+    return (
+      <Box className="w-96 login-container">
+        <img src={imageURL} alt="Login" className='img' />
+         { account==='login'?
+                        <Box className='wrapper'>
+                          <TextField id="standard-basic" label="Enter username" variant="standard" />
+                          <TextField id="standard-basic" label="Enter password" variant="standard" />
+                          <Button variant="contained" className='login-btn'>Login</Button>
+                          <p className='text-center  text-sm m-2'>OR</p>
+                          <Button variant="text" className='signup-btn' onClick={signUpHandler}>CREATE AN ACCOUNT</Button>
+                        </Box>
+                    :
+                        <Box className='wrapper'>
+                          <TextField id="standard-basic" label="Enter Name" variant="standard" />
+                          <TextField id="standard-basic" label="Enter username" variant="standard" />
+                          <TextField id="standard-basic" label="Enter password" variant="standard" />
+                          <Button variant="contained" className='login-btn'>Sign Up</Button>
+                          <p className='text-center  text-sm m-2'>OR</p>
+                          <Button variant="text" className='signup-btn' onClick={signUpHandler}>ALREADY HAVE AN ACCOUNT</Button>
+                        </Box>
+        }
+      </Box>
+    );
 }
+
