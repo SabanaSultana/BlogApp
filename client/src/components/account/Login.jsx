37b6548566a4ffa93e5f385d32793d4194b4ac5f
Sabana <sabanasultana132@gmail.com>
2024-05-01 03:33:56 +0530

    "style: changed color of  login button, added padding "

--- client/src/components/account/Login.jsx
+++ client/src/components/account/Login.jsx
@@ -5,14 +5,14 @@ import "./Login.css";
 export default function Login() {
   const imageURL = 'https://www.sesta.it/wp-content/uploads/2021/03/logo-blog-sesta-trasparente.png';
   return (
-   <Box className="container">
+   <Box className="w-96 login-container">
      <img src={imageURL} alt="Login"  className='img'/>
      <Box className='wrapper'>
-      <TextField id="standard-basic" label="Standard" variant="standard" />
-      <TextField id="standard-basic" label="Standard" variant="standard" />
-      <Button variant="contained" className='btn'>LOGIN</Button>
-      <p>OR</p>
-      <Button variant="text" className='btn'>CREATE AN ACCOUNT</Button>
+      <TextField id="standard-basic"  variant="standard" />
+      <TextField id="standard-basic"  variant="standard" />
+      <Button variant="contained" className='btn '>Login</Button>
+      <p className='text-center pt-2'>OR</p>
+      <Button variant="text" className=''>CREATE AN ACCOUNT</Button>
      </Box>
    </Box>
   )
