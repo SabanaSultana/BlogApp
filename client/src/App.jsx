9a02ca492529caac03f8af48060a7f106f225a40
Sabana <sabanasultana132@gmail.com>
2024-05-01 01:53:19 +0530

    created react-app

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     Hey I am Using react-app
+    </>
+  )
+}
+
+export default App
